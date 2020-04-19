78cff8b2641b2225c48999ee750bb8f19efe7a70
manyi5261 <45423434+manyi5261@users.noreply.github.com>
2020-04-19 17:34:54 +0800

    manyih5
    manyih5

---
+++ maccmsstatic/js/tj.js
@@ -0,0 +1,10 @@
+document.write('<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143287851-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag(\'js\', new Date());
+
+  gtag(\'config\', \'UA-143287851-3\');
+</script>
+<script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1278826435&web_id=1278826435"></script>')
